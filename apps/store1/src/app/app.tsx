95d86255514bfc3b3c7c7bd20e5fbd2af86a845d
ashishsng7 <ashish.singh@techolution.com>
2024-08-12 20:07:41 +0530

    route-config

--- apps/store1/src/app/app.tsx
+++ apps/store1/src/app/app.tsx
@@ -1,16 +1,44 @@
-import styled from 'styled-components';
-
 import NxWelcome from './nx-welcome';
+import { Products } from '@react-monorepo2/products';
+import {
+  BrowserRouter,
+  Route,
+  Routes,
+  Navigate,
+  NavLink,
+} from 'react-router-dom';
 
-const StyledApp = styled.div`
-  // Your style here
-`;
+export function Home({ title, path }: { title: string; path?: string }) {
+  if (!path) {
+    return <Navigate to="/" />;
+  }
+  return (
+    <div>
+      <NavLink
+        to={`/${path}`}
+        className={({ isActive, isPending }) =>
+          isPending ? 'pending' : isActive ? 'active' : ''
+        }
+      >
+        {path}
+      </NavLink>
+      <NxWelcome title={title} />
+    </div>
+  );
+}
 
 export function App() {
   return (
-    <StyledApp>
-      <NxWelcome title="store1" />
-    </StyledApp>
+    <BrowserRouter>
+      <Routes>
+        <Route path="/" element={<Home title="react-monorepo" path="home" />} />
+        <Route
+          path="/home"
+          element={<Home title="store1" path="products" />}
+        ></Route>
+        <Route path="/products" element={<Products />}></Route>
+      </Routes>
+    </BrowserRouter>
   );
 }
 
--- apps/store2/rspack.config.js
+++ apps/store2/rspack.config.js
@@ -1,10 +1,5 @@
 const { composePlugins, withNx, withReact } = require('@nx/rspack');
 
 module.exports = composePlugins(withNx(), withReact(), (config) => {
-  config.devServer = {
-    ...config.devServer,
-    historyApiFallback: true,
-  };
-
   return config;
 });
